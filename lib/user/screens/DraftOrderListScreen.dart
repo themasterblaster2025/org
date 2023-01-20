7c082a8372e0799a6e1f2d203e966bf8087fa679
fulvatandel <fulva.goldenmace@gmail.com>
2023-01-20 16:09:13 +0530

    - bug fixed

--- lib/user/screens/DraftOrderListScreen.dart
+++ lib/user/screens/DraftOrderListScreen.dart
@@ -151,7 +151,7 @@ class DraftOrderListScreenState extends State<DraftOrderListScreen> {
                                         : Row(
                                             children: [
                                               item.date != null ? Text(printDate(item.date!), style: secondaryTextStyle()).expand() : SizedBox(),
-                                              Text('${printAmount(item.totalAmount.validate())}', style: boldTextStyle()),
+                                              Text('${printAmount(item.totalAmount??0)}', style: boldTextStyle()),
                                             ],
                                           ),
                                     if (item.pickupPoint!.address != null || item.deliveryPoint!.address != null)
