2ef802943eb34d16d3d013109df67911a3e8ddd2
meet03 <kruti.meetmighty@gmail.com>
2024-02-28 12:43:31 +0530

    map added in create orderscreen , google distance matrix api integrated, country code will in create order will be currently selected country

---
+++ lib/main/store/AppStore.g.dart
@@ -0,0 +1,529 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'AppStore.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
+
+mixin _$AppStore on _AppStore, Store {
+  late final _$isLoadingAtom =
+      Atom(name: '_AppStore.isLoading', context: context);
+
+  @override
+  bool get isLoading {
+    _$isLoadingAtom.reportRead();
+    return super.isLoading;
+  }
+
+  @override
+  set isLoading(bool value) {
+    _$isLoadingAtom.reportWrite(value, super.isLoading, () {
+      super.isLoading = value;
+    });
+  }
+
+  late final _$isLoggedInAtom =
+      Atom(name: '_AppStore.isLoggedIn', context: context);
+
+  @override
+  bool get isLoggedIn {
+    _$isLoggedInAtom.reportRead();
+    return super.isLoggedIn;
+  }
+
+  @override
+  set isLoggedIn(bool value) {
+    _$isLoggedInAtom.reportWrite(value, super.isLoggedIn, () {
+      super.isLoggedIn = value;
+    });
+  }
+
+  late final _$userEmailAtom =
+      Atom(name: '_AppStore.userEmail', context: context);
+
+  @override
+  String get userEmail {
+    _$userEmailAtom.reportRead();
+    return super.userEmail;
+  }
+
+  @override
+  set userEmail(String value) {
+    _$userEmailAtom.reportWrite(value, super.userEmail, () {
+      super.userEmail = value;
+    });
+  }
+
+  late final _$allUnreadCountAtom =
+      Atom(name: '_AppStore.allUnreadCount', context: context);
+
+  @override
+  int get allUnreadCount {
+    _$allUnreadCountAtom.reportRead();
+    return super.allUnreadCount;
+  }
+
+  @override
+  set allUnreadCount(int value) {
+    _$allUnreadCountAtom.reportWrite(value, super.allUnreadCount, () {
+      super.allUnreadCount = value;
+    });
+  }
+
+  late final _$selectedLanguageAtom =
+      Atom(name: '_AppStore.selectedLanguage', context: context);
+
+  @override
+  String get selectedLanguage {
+    _$selectedLanguageAtom.reportRead();
+    return super.selectedLanguage;
+  }
+
+  @override
+  set selectedLanguage(String value) {
+    _$selectedLanguageAtom.reportWrite(value, super.selectedLanguage, () {
+      super.selectedLanguage = value;
+    });
+  }
+
+  late final _$isDarkModeAtom =
+      Atom(name: '_AppStore.isDarkMode', context: context);
+
+  @override
+  bool get isDarkMode {
+    _$isDarkModeAtom.reportRead();
+    return super.isDarkMode;
+  }
+
+  @override
+  set isDarkMode(bool value) {
+    _$isDarkModeAtom.reportWrite(value, super.isDarkMode, () {
+      super.isDarkMode = value;
+    });
+  }
+
+  late final _$isFilteringAtom =
+      Atom(name: '_AppStore.isFiltering', context: context);
+
+  @override
+  bool get isFiltering {
+    _$isFilteringAtom.reportRead();
+    return super.isFiltering;
+  }
+
+  @override
+  set isFiltering(bool value) {
+    _$isFilteringAtom.reportWrite(value, super.isFiltering, () {
+      super.isFiltering = value;
+    });
+  }
+
+  late final _$uidAtom = Atom(name: '_AppStore.uid', context: context);
+
+  @override
+  String get uid {
+    _$uidAtom.reportRead();
+    return super.uid;
+  }
+
+  @override
+  set uid(String value) {
+    _$uidAtom.reportWrite(value, super.uid, () {
+      super.uid = value;
+    });
+  }
+
+  late final _$isOtpVerifyOnPickupDeliveryAtom =
+      Atom(name: '_AppStore.isOtpVerifyOnPickupDelivery', context: context);
+
+  @override
+  bool get isOtpVerifyOnPickupDelivery {
+    _$isOtpVerifyOnPickupDeliveryAtom.reportRead();
+    return super.isOtpVerifyOnPickupDelivery;
+  }
+
+  @override
+  set isOtpVerifyOnPickupDelivery(bool value) {
+    _$isOtpVerifyOnPickupDeliveryAtom
+        .reportWrite(value, super.isOtpVerifyOnPickupDelivery, () {
+      super.isOtpVerifyOnPickupDelivery = value;
+    });
+  }
+
+  late final _$currencyCodeAtom =
+      Atom(name: '_AppStore.currencyCode', context: context);
+
+  @override
+  String get currencyCode {
+    _$currencyCodeAtom.reportRead();
+    return super.currencyCode;
+  }
+
+  @override
+  set currencyCode(String value) {
+    _$currencyCodeAtom.reportWrite(value, super.currencyCode, () {
+      super.currencyCode = value;
+    });
+  }
+
+  late final _$currencySymbolAtom =
+      Atom(name: '_AppStore.currencySymbol', context: context);
+
+  @override
+  String get currencySymbol {
+    _$currencySymbolAtom.reportRead();
+    return super.currencySymbol;
+  }
+
+  @override
+  set currencySymbol(String value) {
+    _$currencySymbolAtom.reportWrite(value, super.currencySymbol, () {
+      super.currencySymbol = value;
+    });
+  }
+
+  late final _$availableBalAtom =
+      Atom(name: '_AppStore.availableBal', context: context);
+
+  @override
+  num get availableBal {
+    _$availableBalAtom.reportRead();
+    return super.availableBal;
+  }
+
+  @override
+  set availableBal(num value) {
+    _$availableBalAtom.reportWrite(value, super.availableBal, () {
+      super.availableBal = value;
+    });
+  }
+
+  late final _$isVehicleOrderAtom =
+      Atom(name: '_AppStore.isVehicleOrder', context: context);
+
+  @override
+  int get isVehicleOrder {
+    _$isVehicleOrderAtom.reportRead();
+    return super.isVehicleOrder;
+  }
+
+  @override
+  set isVehicleOrder(int value) {
+    _$isVehicleOrderAtom.reportWrite(value, super.isVehicleOrder, () {
+      super.isVehicleOrder = value;
+    });
+  }
+
+  late final _$userProfileAtom =
+      Atom(name: '_AppStore.userProfile', context: context);
+
+  @override
+  String get userProfile {
+    _$userProfileAtom.reportRead();
+    return super.userProfile;
+  }
+
+  @override
+  set userProfile(String value) {
+    _$userProfileAtom.reportWrite(value, super.userProfile, () {
+      super.userProfile = value;
+    });
+  }
+
+  late final _$currencyPositionAtom =
+      Atom(name: '_AppStore.currencyPosition', context: context);
+
+  @override
+  String get currencyPosition {
+    _$currencyPositionAtom.reportRead();
+    return super.currencyPosition;
+  }
+
+  @override
+  set currencyPosition(String value) {
+    _$currencyPositionAtom.reportWrite(value, super.currencyPosition, () {
+      super.currencyPosition = value;
+    });
+  }
+
+  late final _$invoiceCompanyNameAtom =
+      Atom(name: '_AppStore.invoiceCompanyName', context: context);
+
+  @override
+  String get invoiceCompanyName {
+    _$invoiceCompanyNameAtom.reportRead();
+    return super.invoiceCompanyName;
+  }
+
+  @override
+  set invoiceCompanyName(String value) {
+    _$invoiceCompanyNameAtom.reportWrite(value, super.invoiceCompanyName, () {
+      super.invoiceCompanyName = value;
+    });
+  }
+
+  late final _$invoiceContactNumberAtom =
+      Atom(name: '_AppStore.invoiceContactNumber', context: context);
+
+  @override
+  String get invoiceContactNumber {
+    _$invoiceContactNumberAtom.reportRead();
+    return super.invoiceContactNumber;
+  }
+
+  @override
+  set invoiceContactNumber(String value) {
+    _$invoiceContactNumberAtom.reportWrite(value, super.invoiceContactNumber,
+        () {
+      super.invoiceContactNumber = value;
+    });
+  }
+
+  late final _$invoiceAddressAtom =
+      Atom(name: '_AppStore.invoiceAddress', context: context);
+
+  @override
+  String get invoiceAddress {
+    _$invoiceAddressAtom.reportRead();
+    return super.invoiceAddress;
+  }
+
+  @override
+  set invoiceAddress(String value) {
+    _$invoiceAddressAtom.reportWrite(value, super.invoiceAddress, () {
+      super.invoiceAddress = value;
+    });
+  }
+
+  late final _$invoiceCompanyLogoAtom =
+      Atom(name: '_AppStore.invoiceCompanyLogo', context: context);
+
+  @override
+  String get invoiceCompanyLogo {
+    _$invoiceCompanyLogoAtom.reportRead();
+    return super.invoiceCompanyLogo;
+  }
+
+  @override
+  set invoiceCompanyLogo(String value) {
+    _$invoiceCompanyLogoAtom.reportWrite(value, super.invoiceCompanyLogo, () {
+      super.invoiceCompanyLogo = value;
+    });
+  }
+
+  late final _$distanceUnitAtom =
+      Atom(name: '_AppStore.distanceUnit', context: context);
+
+  @override
+  String get distanceUnit {
+    _$distanceUnitAtom.reportRead();
+    return super.distanceUnit;
+  }
+
+  @override
+  set distanceUnit(String value) {
+    _$distanceUnitAtom.reportWrite(value, super.distanceUnit, () {
+      super.distanceUnit = value;
+    });
+  }
+
+  late final _$setLoadingAsyncAction =
+      AsyncAction('_AppStore.setLoading', context: context);
+
+  @override
+  Future<void> setLoading(bool val) {
+    return _$setLoadingAsyncAction.run(() => super.setLoading(val));
+  }
+
+  late final _$setLoginAsyncAction =
+      AsyncAction('_AppStore.setLogin', context: context);
+
+  @override
+  Future<void> setLogin(bool val, {bool isInitializing = false}) {
+    return _$setLoginAsyncAction
+        .run(() => super.setLogin(val, isInitializing: isInitializing));
+  }
+
+  late final _$setUserEmailAsyncAction =
+      AsyncAction('_AppStore.setUserEmail', context: context);
+
+  @override
+  Future<void> setUserEmail(String val, {bool isInitialization = false}) {
+    return _$setUserEmailAsyncAction
+        .run(() => super.setUserEmail(val, isInitialization: isInitialization));
+  }
+
+  late final _$setUIdAsyncAction =
+      AsyncAction('_AppStore.setUId', context: context);
+
+  @override
+  Future<void> setUId(String val, {bool isInitializing = false}) {
+    return _$setUIdAsyncAction
+        .run(() => super.setUId(val, isInitializing: isInitializing));
+  }
+
+  late final _$setAllUnreadCountAsyncAction =
+      AsyncAction('_AppStore.setAllUnreadCount', context: context);
+
+  @override
+  Future<void> setAllUnreadCount(int val) {
+    return _$setAllUnreadCountAsyncAction
+        .run(() => super.setAllUnreadCount(val));
+  }
+
+  late final _$setOtpVerifyOnPickupDeliveryAsyncAction =
+      AsyncAction('_AppStore.setOtpVerifyOnPickupDelivery', context: context);
+
+  @override
+  Future<void> setOtpVerifyOnPickupDelivery(bool val) {
+    return _$setOtpVerifyOnPickupDeliveryAsyncAction
+        .run(() => super.setOtpVerifyOnPickupDelivery(val));
+  }
+
+  late final _$setCurrencyCodeAsyncAction =
+      AsyncAction('_AppStore.setCurrencyCode', context: context);
+
+  @override
+  Future<void> setCurrencyCode(String val) {
+    return _$setCurrencyCodeAsyncAction.run(() => super.setCurrencyCode(val));
+  }
+
+  late final _$setCurrencySymbolAsyncAction =
+      AsyncAction('_AppStore.setCurrencySymbol', context: context);
+
+  @override
+  Future<void> setCurrencySymbol(String val) {
+    return _$setCurrencySymbolAsyncAction
+        .run(() => super.setCurrencySymbol(val));
+  }
+
+  late final _$setCurrencyPositionAsyncAction =
+      AsyncAction('_AppStore.setCurrencyPosition', context: context);
+
+  @override
+  Future<void> setCurrencyPosition(String val) {
+    return _$setCurrencyPositionAsyncAction
+        .run(() => super.setCurrencyPosition(val));
+  }
+
+  late final _$setLanguageAsyncAction =
+      AsyncAction('_AppStore.setLanguage', context: context);
+
+  @override
+  Future<void> setLanguage(String aCode, {BuildContext? context}) {
+    return _$setLanguageAsyncAction
+        .run(() => super.setLanguage(aCode, context: context));
+  }
+
+  late final _$setDarkModeAsyncAction =
+      AsyncAction('_AppStore.setDarkMode', context: context);
+
+  @override
+  Future<void> setDarkMode(bool aIsDarkMode) {
+    return _$setDarkModeAsyncAction.run(() => super.setDarkMode(aIsDarkMode));
+  }
+
+  late final _$setFilteringAsyncAction =
+      AsyncAction('_AppStore.setFiltering', context: context);
+
+  @override
+  Future<void> setFiltering(bool val) {
+    return _$setFilteringAsyncAction.run(() => super.setFiltering(val));
+  }
+
+  late final _$setUserProfileAsyncAction =
+      AsyncAction('_AppStore.setUserProfile', context: context);
+
+  @override
+  Future<void> setUserProfile(String val, {bool isInitializing = false}) {
+    return _$setUserProfileAsyncAction
+        .run(() => super.setUserProfile(val, isInitializing: isInitializing));
+  }
+
+  late final _$_AppStoreActionController =
+      ActionController(name: '_AppStore', context: context);
+
+  @override
+  void setInvoiceCompanyName(String val) {
+    final _$actionInfo = _$_AppStoreActionController.startAction(
+        name: '_AppStore.setInvoiceCompanyName');
+    try {
+      return super.setInvoiceCompanyName(val);
+    } finally {
+      _$_AppStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setInvoiceCompanyLogo(String val) {
+    final _$actionInfo = _$_AppStoreActionController.startAction(
+        name: '_AppStore.setInvoiceCompanyLogo');
+    try {
+      return super.setInvoiceCompanyLogo(val);
+    } finally {
+      _$_AppStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setInvoiceContactNumber(String val) {
+    final _$actionInfo = _$_AppStoreActionController.startAction(
+        name: '_AppStore.setInvoiceContactNumber');
+    try {
+      return super.setInvoiceContactNumber(val);
+    } finally {
+      _$_AppStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setCompanyAddress(String val) {
+    final _$actionInfo = _$_AppStoreActionController.startAction(
+        name: '_AppStore.setCompanyAddress');
+    try {
+      return super.setCompanyAddress(val);
+    } finally {
+      _$_AppStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setDistanceUnit(String val) {
+    final _$actionInfo = _$_AppStoreActionController.startAction(
+        name: '_AppStore.setDistanceUnit');
+    try {
+      return super.setDistanceUnit(val);
+    } finally {
+      _$_AppStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+isLoading: ${isLoading},
+isLoggedIn: ${isLoggedIn},
+userEmail: ${userEmail},
+allUnreadCount: ${allUnreadCount},
+selectedLanguage: ${selectedLanguage},
+isDarkMode: ${isDarkMode},
+isFiltering: ${isFiltering},
+uid: ${uid},
+isOtpVerifyOnPickupDelivery: ${isOtpVerifyOnPickupDelivery},
+currencyCode: ${currencyCode},
+currencySymbol: ${currencySymbol},
+availableBal: ${availableBal},
+isVehicleOrder: ${isVehicleOrder},
+userProfile: ${userProfile},
+currencyPosition: ${currencyPosition},
+invoiceCompanyName: ${invoiceCompanyName},
+invoiceContactNumber: ${invoiceContactNumber},
+invoiceAddress: ${invoiceAddress},
+invoiceCompanyLogo: ${invoiceCompanyLogo},
+distanceUnit: ${distanceUnit}
+    ''';
+  }
+}
